67304535fc8330e427061a6531743f1bd3001f42
re5nardo <re5nardo@naver.com>
2022-07-13 23:13:17 +0900

    불필요해진 파일들 제거

--- MatchmakingServer/models/response/cancel_matchmaking_ticket_response.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const ResponseBase = require('./response_base');
-
-module.exports = class CancelMatchmakingTicketResponse extends ResponseBase {
-    constructor(code) {
-        super(code);
-    }
-}
--- MatchmakingServer/models/response/create_matchmaking_ticket_response.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const ResponseBase = require('./response_base');
-
-module.exports = class CreateMatchmakingTicketResponse extends ResponseBase {
-    constructor(code, ticketId) {
-        super(code);
-        this.ticketId = ticketId;
-    }
-}
