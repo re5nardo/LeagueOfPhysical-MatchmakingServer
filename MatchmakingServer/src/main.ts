0b4571e9f6c3adba90ef2eef2cb601e535878cc1
re5nardo <re5nardo@naver.com>
2022-07-13 11:49:42 +0900

    route 추가

--- MatchmakingServer/src/main.ts
+++ MatchmakingServer/src/main.ts
@@ -1,5 +1,8 @@
 import App from '@src/app';
 import IndexRoute from '@routes/index.route';
+import MatchmakingRoute from '@routes/matchmaking.route';
+import WaitingRoomRoute from '@routes/waitingRoom.route';
+import MatchmakingTicketRoute from '@routes/matchmakingTicket.route';
 import validateEnv from '@utils/validateEnv';
 import { logger } from '@utils/logger';
 import loader from '@loaders/index';
@@ -10,11 +13,10 @@ import loader from '@loaders/index';
 
         await loader();
 
-        const app = new App([new IndexRoute()]);
+        const app = new App([new IndexRoute(), new MatchmakingRoute(), new WaitingRoomRoute(), new MatchmakingTicketRoute()]);
 
         app.listen();
-
-      } catch (error) {
-          logger.error(`main error. error: ${error}`);
-      }
+    } catch (error) {
+        logger.error(`main error. error: ${error}`);
+    }
 })();
