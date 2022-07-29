00a8f7e858951ce5a46d825083be3ce31c91e714
re5nardo <re5nardo@naver.com>
2022-07-29 18:23:04 +0900

    WaitingRoomRedis TTL 수정

--- MatchmakingServer/src/daos/waitingRoom.dao.redis.ts
+++ MatchmakingServer/src/daos/waitingRoom.dao.redis.ts
@@ -1,7 +1,7 @@
 import { WaitingRoom } from "@interfaces/waitingRoom.interface";
 import { DaoRedisBase } from '@daos/dao.redis.base';
 
-const TTL: number = 5 * 60;  //  sec
+const TTL: number = 5;  //  sec
 const WAITING_ROOM_PREFIX: string = 'WAITING_ROOM_PREFIX';
 
 export class WaitingRoomDaoRedis extends DaoRedisBase<WaitingRoom> {
