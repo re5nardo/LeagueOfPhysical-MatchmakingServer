120a1e103aeb1befe6117aa212aaf1a1b79c5b26
re5nardo <re5nardo@naver.com>
2025-03-01 18:05:44 +0900

    postgres loader 추가 및 적용

--- MatchmakingServer/src/loaders/index.ts
+++ MatchmakingServer/src/loaders/index.ts
@@ -1,4 +1,5 @@
 import * as mongooseLoader from '@loaders/mongoose.loader';
+import * as postgresLoader from '@loaders/postgres.loader'
 import * as redisLoader from '@loaders/redis.loader';
 import * as masterdataLoader from '@loaders/masterdata.loader';
 import { logger } from '@utils/logger';
@@ -6,10 +7,13 @@ import { logger } from '@utils/logger';
 export default async () => {
     try {
         await mongooseLoader.load();
-        logger.info('✌️ DB loaded and connected!');
+        logger.info('✌️ mongoose loaded and connected!');
+
+        await postgresLoader.load();
+        logger.info('✌️ postgres loaded and connected!');
         
         await redisLoader.load();
-        logger.info('✌️ Cache loaded and connected!');
+        logger.info('✌️ redis loaded and connected!');
 
         await masterdataLoader.load();
         logger.info('✌️ MasterData loaded!');
